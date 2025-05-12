711aadb0e62a06fac464d0bc219c324618abb578
Bertrand Darbon <bertrand@hyle.eu>
2025-05-12 14:49:40 +0200

    Revert "🐛 Fix undefined crypto.subtle"
    This reverts commit 45dd0792513b80405ab16752e38b445b771fe31d.

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -4,11 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    target: "esnext",
-  },
   optimizeDeps: {
     esbuildOptions: { target: "esnext" },
-    exclude: ["@noir-lang/noirc_abi", "@noir-lang/acvm_js", "crypto"],
+    exclude: ["@noir-lang/noirc_abi", "@noir-lang/acvm_js"],
   },
 });
