b0c38b9092513558feb9b99264e20179d4894e07
Lancelot de Ferrière <wraitii@gmail.com>
2025-06-10 22:52:44 +0200

    fix config parsing

--- server/src/conf.rs
+++ server/src/conf.rs
@@ -44,9 +44,7 @@ impl Conf {
             .add_source(
                 Environment::with_prefix("hyle")
                     .separator("__")
-                    .prefix_separator("_")
-                    .list_separator(",")
-                    .try_parsing(true),
+                    .prefix_separator("_"),
             )
             .build()?
             .try_deserialize()?;
