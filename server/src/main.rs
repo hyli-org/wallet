386a8431fe4635f1086ac4cce98f9a89fc3c7f3b
Maximilien Grattepanche <167073016+maxgttph@users.noreply.github.com>
2025-06-11 14:22:23 +0200

    Remove smt autoprovers (#19)
    * Remove smt autoprovers
    
    * auto provers optionnally

--- server/src/main.rs
+++ server/src/main.rs
@@ -7,7 +7,7 @@ use client_sdk::{
     rest_client::{IndexerApiHttpClient, NodeApiHttpClient},
 };
 use conf::Conf;
-use history::{HistoryEvent, HyllarHistory};
+use history::{HistoryEvent, TokenHistory};
 use hyle_modules::{
     bus::{metrics::BusMetrics, SharedMessageBus},
     modules::{
@@ -54,6 +54,9 @@ pub struct Args {
 
     #[arg(short, long, default_value = "false")]
     pub mock_invites: bool,
+
+    #[arg(short, long, default_value = "false")]
+    pub auto_provers: bool,
 }
 
 #[tokio::main]
@@ -132,7 +135,7 @@ async fn main() -> Result<()> {
         })
         .await?;
     handler
-        .build_module::<ContractStateIndexer<HyllarHistory, Vec<HistoryEvent>>>(
+        .build_module::<ContractStateIndexer<TokenHistory, Vec<HistoryEvent>>>(
             ContractStateIndexerCtx {
                 contract_name: "oranj".into(),
                 data_directory: config.data_directory.clone(),
@@ -152,53 +155,12 @@ async fn main() -> Result<()> {
             max_txs_per_proof: config.wallet_max_txs_per_proof,
         }))
         .await?;
-    handler
-        .build_module::<AutoProver<SmtTokenProvableState>>(Arc::new(AutoProverCtx {
-            data_directory: config.data_directory.clone(),
-            prover: Arc::new(Risc0Prover::new(
-                hyle_smt_token::client::tx_executor_handler::metadata::SMT_TOKEN_ELF,
-            )),
-            contract_name: "oranj".into(),
-            node: app_ctx.node_client.clone(),
-            default_state: Default::default(),
-            buffer_blocks: config.smt_buffer_blocks,
-            max_txs_per_proof: config.smt_max_txs_per_proof,
-        }))
-        .await?;
-
-    handler
-        .build_module::<AutoProver<SmtTokenProvableState>>(Arc::new(AutoProverCtx {
-            data_directory: config.data_directory.clone(),
-            prover: Arc::new(Risc0Prover::new(
-                hyle_smt_token::client::tx_executor_handler::metadata::SMT_TOKEN_ELF,
-            )),
-            contract_name: "vitamin".into(),
-            node: app_ctx.node_client.clone(),
-            default_state: Default::default(),
-            buffer_blocks: 1,
-            max_txs_per_proof: 30,
-        }))
-        .await?;
-
-    handler
-        .build_module::<AutoProver<SmtTokenProvableState>>(Arc::new(AutoProverCtx {
-            data_directory: config.data_directory.clone(),
-            prover: Arc::new(Risc0Prover::new(
-                hyle_smt_token::client::tx_executor_handler::metadata::SMT_TOKEN_ELF,
-            )),
-            contract_name: "oxygen".into(),
-            node: app_ctx.node_client.clone(),
-            default_state: Default::default(),
-            buffer_blocks: 1,
-            max_txs_per_proof: 30,
-        }))
-        .await?;
 
     handler
         .build_module::<WebSocketModule<AppWsInMessage, AppOutWsEvent>>(config.websocket.clone())
         .await?;
 
-    // This module connects to the da_address and receives all the blocks²
+    // This module connects to the da_address and receives all the blocks
     handler
         .build_module::<DAListener>(DAListenerConf {
             start_block: None,
@@ -206,6 +168,48 @@ async fn main() -> Result<()> {
             da_read_from: config.da_read_from.clone(),
         })
         .await?;
+
+    if args.auto_provers {
+        handler
+            .build_module::<AutoProver<SmtTokenProvableState>>(Arc::new(AutoProverCtx {
+                data_directory: config.data_directory.clone(),
+                prover: Arc::new(Risc0Prover::new(
+                    hyle_smt_token::client::tx_executor_handler::metadata::SMT_TOKEN_ELF,
+                )),
+                contract_name: "oranj".into(),
+                node: app_ctx.node_client.clone(),
+                default_state: Default::default(),
+                buffer_blocks: config.smt_buffer_blocks,
+                max_txs_per_proof: config.smt_max_txs_per_proof,
+            }))
+            .await?;
+        handler
+            .build_module::<AutoProver<SmtTokenProvableState>>(Arc::new(AutoProverCtx {
+                data_directory: config.data_directory.clone(),
+                prover: Arc::new(Risc0Prover::new(
+                    hyle_smt_token::client::tx_executor_handler::metadata::SMT_TOKEN_ELF,
+                )),
+                contract_name: "vitamin".into(),
+                node: app_ctx.node_client.clone(),
+                default_state: Default::default(),
+                buffer_blocks: config.smt_buffer_blocks,
+                max_txs_per_proof: config.smt_max_txs_per_proof,
+            }))
+            .await?;
+        handler
+            .build_module::<AutoProver<SmtTokenProvableState>>(Arc::new(AutoProverCtx {
+                data_directory: config.data_directory.clone(),
+                prover: Arc::new(Risc0Prover::new(
+                    hyle_smt_token::client::tx_executor_handler::metadata::SMT_TOKEN_ELF,
+                )),
+                contract_name: "oxygen".into(),
+                node: app_ctx.node_client.clone(),
+                default_state: Default::default(),
+                buffer_blocks: config.smt_buffer_blocks,
+                max_txs_per_proof: config.smt_max_txs_per_proof,
+            }))
+            .await?;
+    }
 
     if args.mock_invites {
         handler
